ebede0767e7d0363d5407ac0d706ee516b111bae
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2016-04-04 16:26:09 -0400

    Fixing shabang.

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from distutils.core import setup
-setup(name="Keycmd A modal file manager",
-      version="1.0.0",
+setup(name="Keycmd",
+      version="1.0.1",
       packages=["cmdlib", 'cmdlib.modes'],
       scripts=['keycmd'],
       package_data={'cmdlib': ['cmdrc', '/cmdrc/cmdrc']},
       author="Iury O. G. Figueiredo",
       author_email="ioliveira@id.uff.br")
+
+
 
 
 
