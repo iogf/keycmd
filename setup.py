60a7d6ad8807a68a6304537dd2bae36c5aa919ce
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2016-05-07 21:29:00 -0400

    Adding description to setup.py.

--- setup.py
+++ setup.py
@@ -5,7 +5,14 @@ setup(name="Keycmd",
       scripts=['keycmd'],
       package_data={'cmdlib': ['cmdrc', '/cmdrc/cmdrc']},
       author="Iury O. G. Figueiredo",
-      author_email="ioliveira@id.uff.br")
+      author_email="ioliveira@id.uff.br",
+      url='https://github.com/iogf/keycmd',
+      download_url='https://github.com/iogf/keycmd/releases',
+      keywords=['vim', 'vy', 'keycmd', 'filemanager'],
+      classifiers=[],
+      description="A modal/Vim-like file manager. Keycmd is a very flexible file manager")
+
+
 
 
 
